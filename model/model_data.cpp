40f89293b55b1a0586d96767a8dfaed967226fba
SaeedNowroozi <saeednowroozi69@gmail.com>
2023-01-11 11:54:23 +0330

    added QAbstractModel

--- model/model_data.cpp
+++ model/model_data.cpp
@@ -1,7 +1,24 @@
 #include "model_data.h"
 
 Model_Data::Model_Data(QObject *parent)
-    : QObject{parent}
+    : QAbstractListModel{parent}
 {
 
+}
+
+
+int Model_Data::rowCount(const QModelIndex &parent) const
+{
+}
+
+QVariant Model_Data::data(const QModelIndex &index, int role) const
+{
+}
+
+bool Model_Data::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+}
+
+QHash<int, QByteArray> Model_Data::roleNames() const
+{
 }
--- model/model_data.h
+++ model/model_data.h
@@ -1,13 +1,27 @@
 #ifndef MODEL_DATA_H
 #define MODEL_DATA_H
 
+#include <QAbstractListModel>
 #include <QObject>
 
-class Model_Data : public QObject
+class Model_Data : public QAbstractListModel
 {
     Q_OBJECT
 public:
     explicit Model_Data(QObject *parent = nullptr);
+    enum DataRoles {
+            Colored ,
+            Uncolored ,
+            PosX ,
+            PosY,
+    };
+
+    // QAbstractItemModel interface
+public:
+    int rowCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    QHash<int, QByteArray> roleNames() const override;
 };
 
 #endif // MODEL_DATA_H
