400cf102028c7552dec0a9c53b1a3183563a1da2
SaeedNowroozi <saeednowroozi69@gmail.com>
2023-01-19 23:27:05 +0330

    Add zaar algorithm

---
+++ model/zaar_alghorithm.cpp
@@ -0,0 +1,6 @@
+#include "zaar_alghorithm.h"
+
+Zaar_alghorithm::Zaar_alghorithm()
+{
+
+}
---
+++ model/zaar_alghorithm.h
@@ -0,0 +1,11 @@
+#ifndef ZAAR_ALGHORITHM_H
+#define ZAAR_ALGHORITHM_H
+
+
+class Zaar_alghorithm
+{
+public:
+    Zaar_alghorithm();
+};
+
+#endif // ZAAR_ALGHORITHM_H
