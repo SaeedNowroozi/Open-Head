bf6d3332c3f4776407e7f8058ee55be9c619720f
SaeedNowroozi <saeednowroozi69@gmail.com>
2023-04-13 10:29:03 +0330

    add zaar algor function

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-     spdlog::info("Welcome to spdlog!");
+    spdlog::info("Welcome to spdlog!");
     MainWindow w;
     w.show();
     return a.exec();
--- model/zaar_alghorithm.h
+++ model/zaar_alghorithm.h
@@ -1,11 +1,30 @@
 #ifndef ZAAR_ALGHORITHM_H
 #define ZAAR_ALGHORITHM_H
 
+#include <memory>
+#include <model_data.h>
+#include <vector>
+#include <chrono>
+#include <map>
+#include <algorithm>
+#include <exception>
+#include <tuple>
+#include <regex>
 
 class Zaar_alghorithm
 {
 public:
     Zaar_alghorithm();
+    /*
+    Set data for xaar alg to device
+    */
+    std::vector<std::vector<Model_Data>> getdata();
+private:
+    std::shared_ptr<Model_Data> model;
+    /*
+    Get data for xaar alg from model data
+    */
+    void setdata(std::vector<std::vector<Model_Data>> data);
 };
 
 #endif // ZAAR_ALGHORITHM_H
