665d946f9a0537243d382b3fbeb9c28bf970ef6f
SaeedNowroozi <saeednowroozi69@gmail.com>
2023-08-25 00:05:50 +0330

    change model_variable.h

---
+++ build-widget-Desktop_Qt_6_2_2_GCC_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,8 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/saeed/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/saeed/Qt/6.2.2/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/saeed/Qt/6.2.2/gcc_64" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "STRING" "" FORCE)
--- model/model_variable.h
+++ model/model_variable.h
@@ -1,11 +1,52 @@
 #ifndef MODEL_VARIABLE_H
 #define MODEL_VARIABLE_H
 
+#include <tuple>
+#include <map>
+#include <exception>
+#include <stdexcept>
+using namespace std;
 
 class Model_Variable
 {
 public:
     Model_Variable();
+    enum Page {
+        CONTRADICTION_FOUND,
+        SOLUTION_FOUND,
+        KEEP_GOING,
+        CANNOT_PROCEED
+    };
+     Page process_model(bool verbose = true);
+private:
+     enum State : int {
+         UNKNOWN = -3,
+         WHITE = -2,
+         BLACK = -1
+     };
+    std::tuple<std::pair<int,int>, int> dots;
+    class Region {
+    public:
+        Region(const State state, std::tuple<std::pair<int,int>, int> dot)
+            : m_state(state), unknowns(dot) {
+
+            if (state == UNKNOWN) {
+                throw logic_error("LOGIC ERROR: Grid::Region::Region() - state must be known!");
+            }
+            // افزودن یک جفت جدید به تاپل
+            dot = std::make_tuple(std::make_pair(std::get<0>(dot).first, std::get<0>(dot).second), 4);
+                    }
+        bool white() const {
+            return m_state == WHITE;
+        }
+
+        bool black() const {
+            return m_state == BLACK;
+        }
+    private:
+         State m_state;
+        std::tuple<std::pair<int,int>, int> unknowns;
+    };
 };
 
 #endif // MODEL_VARIABLE_H
